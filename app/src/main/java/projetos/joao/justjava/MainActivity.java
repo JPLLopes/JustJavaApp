bada53839b26b4239d2c062cc364e431111872c5
JPLLopes <jpllopes1@hotmail.com>
2018-04-04 23:04:15 +0100

    Added functionality to quantity picker increment and decrement buttons as well as the order button to display the correct price of the order

--- app/src/main/java/projetos/joao/justjava/MainActivity.java
+++ app/src/main/java/projetos/joao/justjava/MainActivity.java
@@ -12,6 +12,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +24,13 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        displayPrice(quantity * 5);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
@@ -41,5 +41,15 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view){
+        quantity++;
+        displayQuantity(quantity);
+    }
+
+    public void decrement(View view){
+        quantity--;
+        displayQuantity(quantity);
     }
 }
