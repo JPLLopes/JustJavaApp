94f5c1032300e700b36338e32aa3f5632185e73f
JPLLopes <jpllopes1@hotmail.com>
2018-04-05 18:12:07 +0100

    modified the layout of the app and added additional text to the UI when the user clicks the order button

--- app/src/main/java/projetos/joao/justjava/MainActivity.java
+++ app/src/main/java/projetos/joao/justjava/MainActivity.java
@@ -24,7 +24,11 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price + "\n";
+
+        priceMessage = priceMessage + "Thank you!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -41,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     public void increment(View view){
